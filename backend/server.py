45b38c519855d64ddd672274307a8f1dc44d7a28
Alan-VSouza <alansouzeesi@gmail.com>
2025-06-23 20:41:48 -0300

    feat: new log to try to catch error

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,23 @@
-from flask import Flask
+import eventlet
+eventlet.monkey_patch()
+
+from flask import Flask, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 @app.route('/')
 def index():
     return "Servidor Socket.IO rodando!"
+
+@socketio.on('connect')
+def handle_connect():
+    print(f"Cliente conectado: {request.sid}")
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Um usuário se desconectou.')
 
 @socketio.on('chat_message')
 def handle_chat_message(data):
@@ -14,4 +25,4 @@ def handle_chat_message(data):
     emit('chat_message', data, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
